39c35162c49752a01013b026218a682c5110e083
Esther <ejidikeesther@gmail.com>
2023-12-08 21:49:47 +0100

    Add Text To Microsoft Logo

--- src/play/boxes.js
+++ src/play/boxes.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import GUI from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+
 
 const gui = new GUI({
     title: 'Tweak Microsoft Logo'
@@ -47,14 +50,12 @@ window.addEventListener('dblclick', () => {
 })
 
 
-const cubeTweaks = gui.addFolder('Cubes')
-cubeTweaks.close()
 const cubeGroupTweaks = gui.addFolder('Cube Group')
 
 
 const group = new THREE.Group
-group.position.y = 0.5
-group.position.x = -0.5
+group.position.y = 0.8
+group.position.x = -1.4
 scene.add(group)
 cubeGroupTweaks.add(group.position, 'x').min(-3).max(3).name('Group Horizontal')
 cubeGroupTweaks.add(group.position, 'y').min(-3).max(3).name('Group Vertical')
@@ -65,66 +66,123 @@ const debugObject = {
     box2Color: '#7FBA00',
     box3Color: '#00A3EE',
     box4Color: '#FFB901',
+    textColor: '#fff'
 }
+
 
 const box1material = new THREE.MeshBasicMaterial({ color: debugObject.box1Color })
 const box1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(0.7, 0.7, 1),
     box1material
 )
-box1.position.x = -0.04
+box1.position.set(0, -0.1, 0)
 group.add(box1)
-cubeTweaks.addColor(debugObject, 'box1Color').onChange(() => {
+const cubeTweaks1 = gui.addFolder('Cube 1')
+cubeTweaks1.addColor(debugObject, 'box1Color').onChange(() => {
     box1material.color.set(debugObject.box1Color)
 })
-cubeTweaks.add(box1.position, 'x').min(-3).max(3).name('Box 1 Horizontal')
-cubeTweaks.add(box1.position, 'y').min(-3).max(3).name('Box 1 Vertical')
+cubeTweaks1.add(box1.position, 'x').min(-3).max(3).name('Position Horizontal')
+cubeTweaks1.add(box1.position, 'y').min(-3).max(3).name('Position Vertical')
+cubeTweaks1.add(box1.scale, 'x').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks1.add(box1.scale, 'y').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks1.close()
 
 
 
 const box2material = new THREE.MeshBasicMaterial({ color: debugObject.box2Color })
 const box2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(0.7, 0.7, 1),
     box2material
 )
 // box2.position.set(1, 0, 0)
-box2.position.set(1.04, 0, 0)
+box2.position.set(0.8, -0.1, 0)
 group.add(box2)
-cubeTweaks.addColor(debugObject, 'box2Color').onChange(() => {
+const cubeTweaks2 = gui.addFolder('Cube 2')
+cubeTweaks2.addColor(debugObject, 'box2Color').onChange(() => {
     box2material.color.set(debugObject.box2Color)
 })
-cubeTweaks.add(box2.position, 'x').min(-3).max(3).name('Box 2 Horizontal')
-cubeTweaks.add(box2.position, 'y').min(-3).max(3).name('Box 2 Vertical')
+cubeTweaks2.add(box1.position, 'x').min(-3).max(3).name('Position Horizontal')
+cubeTweaks2.add(box1.position, 'y').min(-3).max(3).name('Position Vertical')
+cubeTweaks2.add(box1.scale, 'x').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks2.add(box1.scale, 'y').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks2.close()
 
 
 const box3material = new THREE.MeshBasicMaterial({ color: debugObject.box3Color })
 const box3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(0.7, 0.7, 1),
     box3material
 )
 //box3.position.set(0, -1, 0)
-box3.position.set(-0.04, -1.08, 0)
+box3.position.set(0, -0.9, 0)
 group.add(box3)
-cubeTweaks.addColor(debugObject, 'box3Color').onChange(() => {
+const cubeTweaks3 = gui.addFolder('Cube 3')
+cubeTweaks3.addColor(debugObject, 'box3Color').onChange(() => {
     box3material.color.set(debugObject.box3Color)
 })
-cubeTweaks.add(box3.position, 'x').min(-3).max(3).name('Box 3 Horizontal')
-cubeTweaks.add(box3.position, 'y').min(-3).max(3).name('Box 3 Vertical')
+cubeTweaks3.add(box1.position, 'x').min(-3).max(3).name('Position Horizontal')
+cubeTweaks3.add(box1.position, 'y').min(-3).max(3).name('Position Vertical')
+cubeTweaks3.add(box1.scale, 'x').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks3.add(box1.scale, 'y').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks3.close()
 
 
 const box4material = new THREE.MeshBasicMaterial({ color: debugObject.box4Color })
 const box4 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(0.7, 0.7, 1),
     box4material
 )
 // box4.position.set(1, -1, 0)
-box4.position.set(1.04, -1.08, 0)
+box4.position.set(0.8, -0.9, 0)
 group.add(box4)
-cubeTweaks.addColor(debugObject, 'box4Color').onChange(() => {
+const cubeTweaks4 = gui.addFolder('Cube 4')
+cubeTweaks4.addColor(debugObject, 'box4Color').onChange(() => {
     box4material.color.set(debugObject.box4Color)
 })
-cubeTweaks.add(box4.position, 'x').min(-3).max(3).name('Box 4 Horizontal')
-cubeTweaks.add(box4.position, 'y').min(-3).max(3).name('Box 4 Vertical')
+cubeTweaks4.add(box1.position, 'x').min(-3).max(3).name('Position Horizontal')
+cubeTweaks4.add(box1.position, 'y').min(-3).max(3).name('Position Vertical')
+cubeTweaks4.add(box1.scale, 'x').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks4.add(box1.scale, 'y').min(-3).max(3).name('Stretch Horizontal')
+cubeTweaks4.close()
+
+
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Microsoft',
+            {
+                font: font,
+                size: 0.7,
+                height: 0.1,
+                curveSegments: 3,
+                bevelEnabled: true,
+                bevelThickness: 0,
+                bevelSize: 0,
+                // bevelOffset: 0,
+                // bevelSegments: 3
+            }
+        )
+        //textGeometry.center()
+        const textMaterial = new THREE.MeshBasicMaterial({ color: debugObject.textColor})
+        const textMesh = new THREE.Mesh(
+            textGeometry,
+            textMaterial
+        )
+        textMesh.position.x = -0.1
+        const text = gui.addFolder('Text')
+        text.addColor(debugObject, 'textColor').onChange(() => {
+            textMaterial.color.set(debugObject.textColor)
+        })
+        text.add(textMesh.position, 'x').min(-3).max(3).name('Position Horizontal')
+        text.add(textMesh.position, 'y').min(-3).max(3).name('Position Vertical')
+        text.add(textMesh.scale, 'x').min(-3).max(3).name('Stretch Horizontal')
+        text.add(textMesh.scale, 'y').min(-3).max(3).name('Stretch Horizontal')
+        text.close()
+        scene.add(textMesh)
+    }
+)
 
 
 const sizes = {
